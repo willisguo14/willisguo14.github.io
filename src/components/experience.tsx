89d70a3aea87fc4bbdc473375372119c138aaf07
willisguo <willisguo02@gmail.com>
2025-01-16 10:39:19 -0500

    update

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -10,7 +10,7 @@ const Experience = () => {
                 <div className='group flex flex-row items-stretch justify-start gap-5 px-2 py-1 rounded-xl transition hover:bg-red-100'>
                     {/* image */}
                     <div className='min-w-[71px] relative bg-red-100 p-2 rounded-lg flex items-center justify-center aspect-square'>
-                        <Image src='/mld.png' alt='CMU MLD' width={50} height={50} radius="none"/>
+                        <Image src='/mld.png' alt='CMU MLD' width={50} height={50} radius="none" />
                     </div>
                     {/* description */}
                     <div className='flex flex-col items-start justify-start p-2 gap-1'>
@@ -18,7 +18,7 @@ const Experience = () => {
                             {/* <div className='min-w-[18px] relative flex items-center justify-center aspect-square'>
                                 <Image src='/clock.svg' alt='language' fill />
                             </div> */}
-                            <p className='text-sm font-medium'>Sep. 2024 - Now</p>
+                            <p className='text-sm font-medium'>Sep. 24' - Present</p>
                         </div>
 
                         <h1 className='text-lg font-medium transition group-hover:text-red-600'>
@@ -39,12 +39,12 @@ const Experience = () => {
                 <div className='group flex flex-row items-stretch justify-start gap-5 px-2 py-1 rounded-xl transition hover:bg-orange-100'>
                     {/* image */}
                     <div className='min-w-[71px] relative bg-orange-100 p-2 rounded-lg flex items-center justify-center aspect-square'>
-                        <Image src='/aws.svg' alt='language' width={50} height={50} radius="none"/>
+                        <Image src='/aws.svg' alt='language' width={50} height={50} radius="none" />
                     </div>
                     {/* description */}
                     <div className='flex flex-col items-start justify-start p-2 gap-1'>
                         <div className='flex flex-row items-center justify-start gap-1 rounded-md bg-transparent pl-1.5 pr-2 py-0.5 border border-slate-200 transition group-hover:border-orange-600'>
-                            <p className='text-sm font-medium'>July - Aug. 2024</p>
+                            <p className='text-sm font-medium'>Jul. 24' - Aug. 24'</p>
                         </div>
                         <h1 className='text-lg font-medium transition group-hover:text-orange-600'>
                             SDE Intern @ Amazon Web Services
@@ -63,12 +63,12 @@ const Experience = () => {
                 <div className='group flex flex-row items-stretch justify-start gap-5 px-2 py-1 rounded-xl transition hover:bg-sky-100'>
                     {/* image */}
                     <div className='min-w-[71px] relative bg-sky-100 p-2 rounded-lg flex items-center justify-center aspect-square'>
-                        <Image src='/uoft-leaf.png' alt='language' width={50} height={50} radius="none"/>
+                        <Image src='/uoft-leaf.png' alt='language' width={50} height={50} radius="none" />
                     </div>
                     {/* description */}
                     <div className='flex flex-col items-start justify-start p-2 gap-1'>
                         <div className='flex flex-row items-center justify-start gap-1 rounded-md bg-transparent pl-1.5 pr-2 py-0.5 border border-slate-200 transition group-hover:border-sky-600'>
-                            <p className='text-sm font-medium'>Sep. 2023 - Apr. 2024</p>
+                            <p className='text-sm font-medium'>Sep. 23' - Apr. 24'</p>
                         </div>
 
                         <h1 className='text-lg font-medium transition group-hover:text-sky-600'>Research Intern, LLMs @ D3M Lab</h1>
@@ -86,12 +86,12 @@ const Experience = () => {
                 <div className='group flex flex-row items-stretch justify-start gap-5 px-2 py-1 rounded-xl transition hover:bg-orange-100'>
                     {/* image */}
                     <div className='min-w-[71px] relative bg-orange-100 p-2 rounded-lg flex items-center justify-center aspect-square'>
-                        <Image src='/aws.svg' alt='language' width={50} height={50} radius="none"/>
+                        <Image src='/aws.svg' alt='language' width={50} height={50} radius="none" />
                     </div>
                     {/* description */}
                     <div className='flex flex-col items-start justify-start p-2 gap-1'>
                         <div className='flex flex-row items-center justify-start gap-1 rounded-md bg-transparent pl-1.5 pr-2 py-0.5 border border-slate-200 transition group-hover:border-orange-600'>
-                            <p className='text-sm font-medium'>June - Aug. 2023</p>
+                            <p className='text-sm font-medium'>Jun. 23' - Aug. 23'</p>
                         </div>
 
                         <h1 className='text-lg font-medium transition group-hover:text-orange-600'>
@@ -110,12 +110,12 @@ const Experience = () => {
                 <div className='group flex flex-row items-stretch justify-start gap-5 px-2 py-1 rounded-xl transition hover:bg-blue-100'>
                     {/* image */}
                     <div className='min-w-[71px] relative bg-blue-100 p-2 rounded-lg flex items-center justify-center aspect-square'>
-                        <Image src='/autoronto.png' alt='language' width={50} height={50} radius="none"/>
+                        <Image src='/autoronto.png' alt='language' width={50} height={50} radius="none" />
                     </div>
                     {/* description */}
                     <div className='flex flex-col items-start justify-start p-2 gap-1'>
                         <div className='flex flex-row items-center justify-start gap-1 rounded-md bg-transparent pl-1.5 pr-2 py-0.5 border border-slate-200 transition group-hover:border-blue-600'>
-                            <p className='text-sm font-medium'>Sep. 2021 - Apr. 2022</p>
+                            <p className='text-sm font-medium'>Sep. 21' - Apr. 22'</p>
                         </div>
 
                         <h1 className='text-lg font-medium transition group-hover:text-blue-600'>ML Engineer @ aUToronto</h1>
@@ -131,12 +131,12 @@ const Experience = () => {
                 <div className='group flex flex-row items-stretch justify-start gap-5 px-2 py-1 rounded-xl transition hover:bg-sky-100'>
                     {/* image */}
                     <div className='min-w-[71px] relative bg-sky-100 p-2 rounded-lg flex items-center justify-center aspect-square'>
-                        <Image src='/uoft-leaf.png' alt='language' width={50} height={50} radius="none"/>
+                        <Image src='/uoft-leaf.png' alt='language' width={50} height={50} radius="none" />
                     </div>
                     {/* description */}
                     <div className='flex flex-col items-start justify-start p-2 gap-1'>
                         <div className='flex flex-row items-center justify-start gap-1 rounded-md bg-transparent pl-1.5 pr-2 py-0.5 border border-slate-200 transition group-hover:border-sky-600'>
-                            <p className='text-sm font-medium'>May - Aug. 2021</p>
+                            <p className='text-sm font-medium'>May 21' - Aug. 21'</p>
                         </div>
                         <h1 className='text-lg font-medium transition group-hover:text-sky-600'>Research Intern, ML for SWE @ FORCOLAB</h1>
 
