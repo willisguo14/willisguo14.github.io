8f7377b0d16e4844202b2c33a089c821ff73ee58
willisguo <willisguo02@gmail.com>
2025-01-19 12:48:05 -0500

    update

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -12,8 +12,7 @@ const About = () => {
             <div className='mx-2 flex flex-col justify-start items-start gap-3'>
                 <h1 className='text-2xl font-medium mb-3'>Hi! I'm Willis</h1>
                 <p className='leading-7'>
-                    I’m a first-year Master’s in Machine Learning student at Carnegie Mellon University, where I am advised by 
-                    <Link href="https://www.cs.cmu.edu/~rsalakhu/" underline="hover" isExternal>Ruslan Salakhutdinov</Link>. My current research interest is improving vision-language models as multimodal dialogue agents. 
+                    I’m a first-year Master’s in Machine Learning student at Carnegie Mellon University, where I am advised by <Link href="https://www.cs.cmu.edu/~rsalakhu/" underline="hover" isExternal>Ruslan Salakhutdinov</Link>. My current research interest is improving vision-language models as multimodal dialogue agents.
                 </p>
                 <p className="leading-7">
                     Previously, I completed my BASc in Machine Intelligence at the University of Toronto, where I worked on neuro-symbolic reasoning with LLMs and LLM-augmented knowledge graph question answering (KGQA), advised by <Link href="https://d3m.mie.utoronto.ca/" underline="hover" isExternal>Scott Sanner</Link>.
